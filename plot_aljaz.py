b806cc9ecc1e7decf3e21151fd82e61732cd15ec
James Brind <jb753@cam.ac.uk>
2021-11-20 13:58:37 +0000

    Mass average unsteady effy, implement blockage

--- plot_aljaz.py
+++ plot_aljaz.py
@@ -5,34 +5,38 @@ import json
 
 # Helper function to read a json file and return a dict
 def load_json(fname):
-    with open(fname,'r') as f:
+    with open(fname, "r") as f:
         data = json.load(f)
     return data
 
+
 # Get all metadata files and read the json
 meta, params = [
-        [load_json(fi) for fi in glob.glob('aljaz/**/%s.json'%si)]
-        for si in ('meta', 'params')
-        ]
+    [load_json(fi) for fi in glob.glob("aljaz/**/%s.json" % si)]
+    for si in ("meta", "params")
+]
 
 # Extract variables to plot
-phi = np.array([pi['mean-line']['phi'] for pi in params])
-psi = np.array([pi['mean-line']['psi'] for pi in params])
-eta_isen = np.array([mi['eff_isen'] for mi in meta])
-eta_isen_lost = np.array([mi['eff_isen_lost'] for mi in meta])
-run_id = ["".join(mi['runid']) for mi in meta]
-err_eta = eta_isen - (1.-eta_isen_lost[:,3])
+phi = np.array([pi["mean-line"]["phi"] for pi in params])
+psi = np.array([pi["mean-line"]["psi"] for pi in params])
+eta_isen = np.array([mi["eff_isen"] for mi in meta])
+eta_isen_lost = np.array([mi["eff_isen_lost"] for mi in meta])
+run_id = ["".join(mi["runid"]) for mi in meta]
+err_eta = eta_isen - (1.0 - eta_isen_lost[:, 3])
 
-print('EFFICIENCY ERRORS')
+print("EFFICIENCY ERRORS")
 for ri, phii, psii, erri in zip(run_id, phi, psi, err_eta):
-    print('Run %s, phi/psi %.2f,%.2f, effy err %.2f percent' %(ri, phii, psii, erri*100.))
+    print(
+        "Run %s, phi/psi %.2f,%.2f, effy err %.2f percent"
+        % (ri, phii, psii, erri * 100.0)
+    )
 
 
 # Make a smith chart
 f, a = plt.subplots()
-cont = a.tricontourf(phi,psi,eta_isen)
-a.set_xlabel('Flow Coefficient')
-a.set_ylabel('Stage Loading Coefficient')
+cont = a.tricontourf(phi, psi, eta_isen)
+a.set_xlabel("Flow Coefficient")
+a.set_ylabel("Stage Loading Coefficient")
 cb = f.colorbar(cont)
-cb.set_label('Isentropic Efficiency')
-plt.savefig('smith.pdf')
+cb.set_label("Isentropic Efficiency")
+plt.savefig("smith.pdf")
--- write_dbslice.py
+++ write_dbslice.py
@@ -10,30 +10,44 @@ Tref = 300.0
 # Choose which variables to write out
 varnames = ["x", "rt", "eff_lost", "pfluc"]
 
+def save_meta(meta, basedir):
+
+    # Save the metadata (lists because numpy arrays not seralisable)
+    for k in meta:
+        try:
+            meta[k][0]
+            meta[k] = list(meta[k])
+        except IndexError:
+            pass
+
+    with open(os.path.join(basedir, "meta.json"), "w") as f:
+        json.dump(meta, f)
+
 def node_to_cell(cut, prop_name):
     return np.mean(
         np.stack(
             (
-                getattr(cut,prop_name)[:-1,:-1],
-                getattr(cut,prop_name)[1:,1:],
-                getattr(cut,prop_name)[:-1,1:],
-                getattr(cut,prop_name)[1:,:-1],
+                getattr(cut, prop_name)[:-1, :-1],
+                getattr(cut, prop_name)[1:, 1:],
+                getattr(cut, prop_name)[:-1, 1:],
+                getattr(cut, prop_name)[1:, :-1],
             )
-        ), axis=0
+        ),
+        axis=0,
     )
 
 
 def cell_vec(c):
-    return c[1:,1:] - c[:-1,:-1] , c[:-1,1:] - c[1:,:-1]
+    return c[1:, 1:] - c[:-1, :-1], c[:-1, 1:] - c[1:, :-1]
+
 
 def cell_area(cut):
-
     dx1, dx2 = cell_vec(cut.x)
     dr1, dr2 = cell_vec(cut.r)
     drt1, drt2 = cell_vec(cut.rt)
 
-    Ax =  0.5*(dr1*drt2 - dr2*drt1)
-    Ar =  0.5*(dx2*drt1 - dx1*drt2)
+    Ax = 0.5 * (dr1 * drt2 - dr2 * drt1)
+    Ar = 0.5 * (dx2 * drt1 - dx1 * drt2)
 
     return Ax, Ar
 
@@ -42,40 +56,52 @@ def mix_out(cuts):
 
     cp = cuts[0].cp
     ga = cuts[0].ga
-    rgas = cp * (ga-1.)/ga
+    rgas = cp * (ga - 1.0) / ga
     cv = cp / ga
 
-    props = ['ro', 'rovx', 'rovr', 'rorvt', 'roe', 'pstat', 'vx','r','vr', 'vt','tstag']
-    fluxes = ['mass', 'xmom', 'rmom', 'tmom', 'energy']
+    props = [
+        "ro",
+        "rovx",
+        "rovr",
+        "rorvt",
+        "roe",
+        "pstat",
+        "vx",
+        "r",
+        "vr",
+        "vt",
+        "tstag",
+    ]
+    fluxes = ["mass", "xmom", "rmom", "tmom", "energy"]
 
     # Preallocate totals
-    total = {f: 0. for f in fluxes}
-    total['Ax'] = 0.
-    total['Ar'] = 0.
+    total = {f: 0.0 for f in fluxes}
+    total["Ax"] = 0.0
+    total["Ar"] = 0.0
 
     # Loop over cuts
     for cut in cuts:
 
         # Cell centered primary properties
-        cell = { prop: node_to_cell(cut, prop) for prop in props }
+        cell = {prop: node_to_cell(cut, prop) for prop in props}
 
         # Cell areas
         Ax, Ar = cell_area(cut)
 
         # Fluxes of the non-uniform flow
         flux_x = {
-            'mass' : cell['rovx'],
-            'xmom' : cell['rovx'] * cell['vx'] + cell['pstat'],
-            'rmom' : cell['rovx'] * cell['vr'],
-            'tmom' : cell['rovx'] * cell['r'] * cell['vt'],
-            'energy' : cell['rovx'] * cell['tstag'],
+            "mass": cell["rovx"],
+            "xmom": cell["rovx"] * cell["vx"] + cell["pstat"],
+            "rmom": cell["rovx"] * cell["vr"],
+            "tmom": cell["rovx"] * cell["r"] * cell["vt"],
+            "energy": cell["rovx"] * cell["tstag"],
         }
         flux_r = {
-            'mass' : cell['rovr'],
-            'xmom' : cell['rovr'] * cell['vx'],
-            'rmom' : cell['rovr'] * cell['vr'] + cell['pstat'],
-            'tmom' : cell['rovr'] * cell['r'] * cell['vt'],
-            'energy' : cell['rovr'] * cell['tstag'],
+            "mass": cell["rovr"],
+            "xmom": cell["rovr"] * cell["vx"],
+            "rmom": cell["rovr"] * cell["vr"] + cell["pstat"],
+            "tmom": cell["rovr"] * cell["r"] * cell["vt"],
+            "energy": cell["rovr"] * cell["tstag"],
         }
 
         # Multiply by area and accumulate totals
@@ -83,8 +109,8 @@ def mix_out(cuts):
             total[f] += np.sum(flux_x[f] * Ax) + np.sum(flux_r[f] * Ar)
 
         # Accumulate areas
-        total['Ax'] += np.sum(Ax)
-        total['Ar'] += np.sum(Ar)
+        total["Ax"] += np.sum(Ax)
+        total["Ar"] += np.sum(Ar)
 
     # Now we solve for the state of mixed out flow assuming constant area
 
@@ -92,39 +118,41 @@ def mix_out(cuts):
     rmid = np.mean((cut.r.min(), cut.r.max()))
 
     # Guess for density
-    mix = { 'ro' :  np.mean(cell['ro']) }
+    mix = {"ro": np.mean(cell["ro"])}
 
     # Iterate on density
     for i in range(10):
 
         # Conservation of mass to get mixed out axial velocity
-        mix['vx'] = total['mass'] / mix['ro'] / total['Ax']
+        mix["vx"] = total["mass"] / mix["ro"] / total["Ax"]
 
         # Conservation of axial momentum to get mixed out static pressure
-        mix['pstat'] = ( total['xmom'] - mix['ro'] * mix['vx']**2. * total['Ax'] ) / total['Ax']
+        mix["pstat"] = (
+            total["xmom"] - mix["ro"] * mix["vx"] ** 2.0 * total["Ax"]
+        ) / total["Ax"]
 
         # Conservation of tangential momentum to get mixed out tangential velocity
-        mix['vt'] =  total['tmom'] / mix['ro'] / mix['vx'] / total['Ax'] / rmid
+        mix["vt"] = total["tmom"] / mix["ro"] / mix["vx"] / total["Ax"] / rmid
 
         # Destruction of radial momentum
-        mix['vr'] = 0.
+        mix["vr"] = 0.0
 
         # Total temperature from first law of thermodynamics
-        mix['tstag'] = total['energy'] / total['mass']
+        mix["tstag"] = total["energy"] / total["mass"]
 
         # Velocity magnitude
-        mix['vabs'] = np.sqrt(mix['vx'] **2. + mix['vr'] **2. + mix['vt'] **2. )
+        mix["vabs"] = np.sqrt(mix["vx"] ** 2.0 + mix["vr"] ** 2.0 + mix["vt"] ** 2.0)
 
         # Lookup compressible flow relation
-        V_cpTo = mix['vabs'] / np.sqrt(cp * mix['tstag'])
-        Ma = np.sqrt( V_cpTo **2. / (ga - 1.) / (1. - 0.5 * V_cpTo**2.))
-        To_T = 1. + 0.5 * (ga - 1.0) * Ma ** 2.;
+        V_cpTo = mix["vabs"] / np.sqrt(cp * mix["tstag"])
+        Ma = np.sqrt(V_cpTo ** 2.0 / (ga - 1.0) / (1.0 - 0.5 * V_cpTo ** 2.0))
+        To_T = 1.0 + 0.5 * (ga - 1.0) * Ma ** 2.0
 
         # Get static T
-        mix['tstat'] = mix['tstag'] / To_T;
+        mix["tstat"] = mix["tstag"] / To_T
 
         # Record mixed out flow condition in primary flow variables
-        mix['ro'] = mix['pstat'] / (rgas * mix['tstat'])
+        mix["ro"] = mix["pstat"] / (rgas * mix["tstat"])
 
     # Max a new cut with the mixed out flow
     cut_out = ts_tstream_cut.TstreamStructuredCut()
@@ -143,33 +171,39 @@ def mix_out(cuts):
     cut_out.x = np.mean(cut.x)
     cut_out.r = rmid
     cut_out.rt = np.mean(cut.rt)
-    cut_out.ro = mix['ro']
-    cut_out.rovx = mix['ro'] * mix['vx']
-    cut_out.rovr = mix['ro'] * mix['vr']
-    cut_out.rorvt = mix['ro'] * mix['vt'] * rmid
-    cut_out.roe = mix['ro'] * (cv * mix['tstat'] + 0.5 * mix['vabs']**2.)
+    cut_out.ro = mix["ro"]
+    cut_out.rovx = mix["ro"] * mix["vx"]
+    cut_out.rovr = mix["ro"] * mix["vr"]
+    cut_out.rorvt = mix["ro"] * mix["vt"] * rmid
+    cut_out.roe = mix["ro"] * (cv * mix["tstat"] + 0.5 * mix["vabs"] ** 2.0)
 
-    cut_out.tstat = mix['tstat']
-    cut_out.tstag = mix['tstag']
-    cut_out.pstat = mix['pstat']
+    cut_out.tstat = mix["tstat"]
+    cut_out.tstag = mix["tstag"]
+    cut_out.pstat = mix["pstat"]
 
-    cut_out.vx = mix['vx']
-    cut_out.vr = mix['vr']
-    cut_out.vt = mix['vt']
-    cut_out.U = rmid * cut_out.rpm / 60. * 2. * np.pi
-    cut_out.vt_rel = mix['vt'] - cut_out.U
+    cut_out.vx = mix["vx"]
+    cut_out.vr = mix["vr"]
+    cut_out.vt = mix["vt"]
+    cut_out.vabs = mix["vabs"]
+    cut_out.U = rmid * cut_out.rpm / 60.0 * 2.0 * np.pi
+    cut_out.vt_rel = mix["vt"] - cut_out.U
 
-    cut_out.mach = mix['vabs'] / np.sqrt(ga * rgas * mix['tstat'])
+    cut_out.vabs_rel = np.sqrt(cut_out.vx**2. + cut_out.vr**2. + cut_out.vt_rel**2.)
+    cut_out.mach_rel = cut_out.vabs_rel / np.sqrt(ga * rgas * cut_out.tstat)
+
+    cut_out.mach = cut_out.vabs / np.sqrt(ga * rgas * cut_out.tstat)
     cut_out.pstag = compflow.Po_P_from_Ma(cut_out.mach, ga) * cut_out.pstat
-
+    cut_out.pstag_rel = compflow.Po_P_from_Ma(cut_out.mach_rel, ga) * cut_out.pstat
 
     return cut_out
+
 
 def average_cuts(cuts, var_name):
     mass, prop = zip(*[ci.mass_avg_1d(var_name) for ci in cuts])
     mass = np.array(mass)
     prop = np.array(prop)
-    return np.sum(mass*prop)/np.sum(mass)
+    return np.sum(mass * prop) / np.sum(mass)
+
 
 def read_dat(fname, shape):
     """Load flow data from a .dat file"""
@@ -202,6 +236,7 @@ def secondary(d, rpm, cp, ga):
     d["vtrel"] = d["vt"] - d["U"]
     d["v"] = np.sqrt(d["vx"] ** 2.0 + d["vr"] ** 2.0 + d["vt"] ** 2.0)
     d["vrel"] = np.sqrt(d["vx"] ** 2.0 + d["vr"] ** 2.0 + d["vtrel"] ** 2.0)
+    d["t"] = d["rt"]/d["r"]
 
     # Total energy for temperature
     E = d["roe"] / d["ro"]
@@ -240,7 +275,7 @@ if __name__ == "__main__":
     print("POST-PROCESSING %s\n" % output_hdf5)
 
     opts = sys.argv[2:]
-    meta_only = '--meta-only' in opts
+    meta_only = "--meta-only" in opts
 
     # Load the grid
     tsr = ts_tstream_reader.TstreamReader()
@@ -257,12 +292,11 @@ if __name__ == "__main__":
     ni = [blki.ni for blki in blk]
     nj = [blki.nj for blki in blk]
     nk = [blki.nk for blki in blk]
-    rpm = np.array([g.get_bv("rpm",bid) for bid in bid_all])
+    rpm = np.array([g.get_bv("rpm", bid) for bid in bid_all])
 
     # Stator/rotor blocks
-    bid_stator = bid_all[rpm==0.]
-    bid_rotor = bid_all[rpm!=0.]
-
+    bid_stator = bid_all[rpm == 0.0]
+    bid_rotor = bid_all[rpm != 0.0]
 
     # Take cuts at inlet/outlet planes of each row
     stator_inlet = []
@@ -326,11 +360,12 @@ if __name__ == "__main__":
         )
 
     # Pull out mass-average flow varibles from the cuts
-    cuts = [mix_out(ci) for ci in [stator_inlet, stator_outlet, rotor_inlet, rotor_outlet]]
-    var_names = ["pstag", "pstat", "tstag", "tstat", "vx", "vt", "vt_rel"]
-    Po, P, To, T, Vx, Vt, Vt_rel = [
-        np.array([getattr(ci, var_name) for ci in cuts])
-        for var_name in var_names
+    cuts = [
+        mix_out(ci) for ci in [stator_inlet, stator_outlet, rotor_inlet, rotor_outlet]
+    ]
+    var_names = ["pstag", "pstat", "tstag", "tstat", "vx", "vt", "vt_rel","pstag_rel"]
+    Po, P, To, T, Vx, Vt, Vt_rel, Po_rel = [
+        np.array([getattr(ci, var_name) for ci in cuts]) for var_name in var_names
     ]
 
     # Calculate entropy change with respect to inlet
@@ -353,23 +388,14 @@ if __name__ == "__main__":
 
     # Lost effy from
     # eta = wx/(wx+Tds) = 1/(1+Tds/wx) approx 1-Tds/wx using Taylor expansion
-    #meta["eff_isen_lost"] = T[3] * ds / cp / (To[0] - To[3])
-    meta["eff_isen_lost"] = 1. - 1./(1. + T[3] * ds / cp / (To[0] - To[3]))
+    # meta["eff_isen_lost"] = T[3] * ds / cp / (To[0] - To[3])
+    meta["eff_isen_lost"] = 1.0 - 1.0 / (1.0 + T[3] * ds / cp / (To[0] - To[3]))
 
     meta["runid"] = run_name
 
-    # Save the metadata (lists because numpy arrays not seralisable)
-    for k in meta:
-        try:
-            meta[k][0]
-            meta[k] = list(meta[k])
-        except IndexError:
-            pass
-
-    with open(os.path.join(basedir, "meta.json"), "w") as f:
-        json.dump(meta, f)
 
     if meta_only:
+        save_meta(meta, basedir)
         quit()
 
     # Determine number of probes
@@ -384,6 +410,9 @@ if __name__ == "__main__":
     vars_all = []
     dijk_all = []
     eff_lost_unst = []
+    mdot_unst = []
+    blockage_unst = []
+    Cp = []
     for bid in g.get_block_ids():
 
         rpm_now = g.get_bv("rpm", bid)
@@ -403,7 +432,7 @@ if __name__ == "__main__":
 
                 fname_now = output_hdf5.replace(
                     ".hdf5", "_probe_%d_%d.dat" % (bid, pid)
-                ).replace("_avg","")
+                ).replace("_avg", "")
 
                 dat_now = read_dat(fname_now, (di, dj, dk))
                 dat_now = secondary(dat_now, rpm_now, cp, ga)
@@ -414,9 +443,41 @@ if __name__ == "__main__":
 
                 # Unsteady lost efficiency at exit if this is a rotor passage
                 if not rpm_now == 0.0:
-                    eff_lost_unst.append(
-                        np.mean(dat_now["eff_lost"][-2, ...], axis=(0, 1))
-                    )
+
+                    # Indexes for LE and TE
+                    pitch = np.diff(dat_now["t"][:,0,(0,-1),0],1,1)
+                    tol = 1e-5
+                    ile = np.where(np.abs(pitch/pitch[0]-1.)>tol)[0][0]-1
+                    ite = np.where(np.abs(pitch/pitch[-1]-1.)>tol)[0][-1]+1
+
+                    # Pressure coefficient
+                    Cp_now = (dat_now["pstat"][ile:(ite+1),0,(0,-1),:]-Po_rel[2])/(Po_rel[2]-P[2])
+                    Cp.append(Cp_now)
+
+                    # Mass average lost effy
+                    rt_now = dat_now["rt"][-2, 0, :, :]
+                    eff_lost_now = dat_now["eff_lost"][-2, 0, :, :]
+                    rovx_now = dat_now["rovx"][-2, 0, :, :]
+                    mdot_now = np.trapz(rovx_now, rt_now, axis=0)
+                    eff_av_now = np.trapz(rovx_now*eff_lost_now, rt_now, axis=0)/mdot_now
+
+                    # blockage
+                    rovx_now = dat_now["rovx"][ile, 0, :, :]
+                    mdot_now = np.trapz(rovx_now, rt_now, axis=0)
+                    rt_now = dat_now["rt"][ile, 0, :, :]
+                    A_now = np.trapz(np.ones_like(rt_now),rt_now,axis=0)
+                    rovx_ref = mdot_now/A_now
+                    blockage_now = np.trapz(1.-rovx_now/rovx_ref, rt_now,axis=0)
+
+                    mdot_unst.append(mdot_now)
+                    eff_lost_unst.append(eff_av_now)
+                    blockage_unst.append(blockage_now)
+
+                    # # For one rotor passage only, look at incidence
+                    # # Mass average inlet tangential momentum
+                    # rorvt_now = dat_now["rorvt"][-2, 0, :, :]
+                    # rorvt_av_now = np.trapz(rorvt_now*rovx_now, rt_now, axis=0)/mdot_now
+
 
                 # Remove variables we are not interested in
                 for k in list(dat_now.keys()):
@@ -436,8 +497,21 @@ if __name__ == "__main__":
                     )
                 )
 
-# Take mean of each rotor passage effy
-meta["eff_lost_unst"] = np.mean(eff_lost_unst, 0)
+Cp = np.array(Cp)
+eff_lost_unst = np.array(eff_lost_unst)
+mdot_unst = np.array(mdot_unst)
+blockage_unst = np.array(blockage_unst)
+
+# Take mass av of each rotor passage effy
+meta["eff_lost_unst"] = (np.sum(eff_lost_unst*mdot_unst, 0)/np.sum(mdot_unst,0))[:]
+meta["mdot_unst"] = np.sum(mdot_unst,0)[:]
+meta["blockage_unst"] = np.mean(blockage_unst,0)[:]
+
+x_c = dat_now["x"][ile:(ite+1),0,0,0]
+x_c = (x_c - x_c[0])/np.ptp(x_c)
+
+# Save with unsteady metadata
+save_meta(meta, basedir)
 
 # Determine number of stators and rotors
 rpms = np.array([g.get_bv("rpm", bidi) for bidi in g.get_block_ids()])
@@ -452,5 +526,6 @@ np.savez_compressed(
     data=var_out,
     sizes=dijk_all,
     nsr=(nstator, nrotor),
+    Cp=Cp,
+    x_c=x_c
 )
-
