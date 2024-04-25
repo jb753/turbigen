977ba736a35fff6fad74e04217694dbddf7be014
James Brind <jb753@cam.ac.uk>
2024-04-25 12:16:32 +0100

    Fix stl bug

--- turbigen/solvers/stl.py
+++ turbigen/solvers/stl.py
@@ -12,7 +12,7 @@ def run(grid, settings, machine):
     workdir = settings["workdir"]
 
     # Extract coordinates
-    sections, annulus, zcst, Nb, tip = machine.get_coords()[:5]
+    sections, annulus, zcst, Nb, tip, splitters = machine.get_coords()
 
     # Write annulus lines
     hub, cas = annulus
@@ -80,6 +80,65 @@ def run(grid, settings, machine):
         stl_name = os.path.join(workdir, f"blade_{iblade}.stl")
         mesh.save(stl_name)
         logger.info(f"Wrote row {iblade} blade xyz to {stl_name}")
+
+    # Write an stl for each splitter
+    for iblade, section in enumerate(splitters):
+        # Join the points at leading edge
+
+        if section[0] is None:
+            continue
+
+        # print(len(section))
+        # print(type(section))
+        # print(type(section[0]))
+        # print(type(section[0]))
+        # print(len(section[0]))
+
+        xrt_ps = section[0]
+        # xrt_ss = np.flip(section[1, :, 1:, :], axis=1)  # Do not repeat LE
+        xrt_ss = np.flip(section[1], axis=1)  # Fix the hole in LE?
+        xrt_section = np.concatenate((xrt_ps, xrt_ss), axis=1)
+
+        xyz_section = np.stack(
+            (
+                xrt_section[..., 0],
+                xrt_section[..., 1] * np.cos(xrt_section[..., 2]),
+                xrt_section[..., 1] * np.sin(xrt_section[..., 2]),
+            ),
+            axis=-1,
+        )
+
+        nj, ni, _ = xrt_section.shape
+        nface = 2 * (ni - 1) * (nj - 1)
+        data = np.zeros(nface, dtype=stl.mesh.Mesh.dtype)
+
+        for i in range(ni - 1):
+            for j in range(nj - 1):
+                # Calculate 1D face indices from 2D node indices
+                kl = 2 * (i + (ni - 1) * j)
+                ku = kl + 1
+                xyz_kl = np.stack(
+                    (
+                        xyz_section[j, i, :],
+                        xyz_section[j, i + 1, :],
+                        xyz_section[j + 1, i + 1, :],
+                    )
+                )
+                xyz_ku = np.stack(
+                    (
+                        xyz_section[j, i, :],
+                        xyz_section[j + 1, i + 1, :],
+                        xyz_section[j + 1, i, :],
+                    )
+                )
+                data["vectors"][kl] = xyz_kl
+                data["vectors"][ku] = xyz_ku
+
+        mesh = stl.mesh.Mesh(data)
+
+        stl_name = os.path.join(workdir, f"splitter_{iblade}.stl")
+        mesh.save(stl_name)
+        logger.info(f"Wrote row {iblade} splitter xyz to {stl_name}")
 
         # ax.add_collection3d(mplot3d.art3d.Poly3DCollection(mesh.vectors))
         # r = np.sqrt(mesh.y**2.+mesh.z**2.)
