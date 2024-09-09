6916b796a5673454230205ab9145bd3e7fe324de
James Brind <jb753@cam.ac.uk>
2024-09-09 18:06:17 +0100

    bench

--- tests/run_bench.sh
+++ tests/run_bench.sh
@@ -8,11 +8,10 @@ unset I_MPI_PMI_LIBRARY
 export OMP_NUM_THREADS=1
 unset PYTHONDONTWRITEBYTECODE
 
-make compile-intel
+make compile
 
 rm -f tests/bench.dat
 
-# for size in 8 4 2 1 ; do
-for size in 8; do
+for size in 8 4 2 1 ; do
     mpirun -np $size python tests/benchmark.py &> /dev/null
 done
