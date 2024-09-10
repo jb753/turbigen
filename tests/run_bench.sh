af81789d5a4392dd54070bddc5c4087f8299083c
James Brind <jb753@cam.ac.uk>
2024-09-10 12:05:12 +0100

    stuff

--- tests/run_bench.sh
+++ tests/run_bench.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-
 if [[ $(hostname) =~ "cpu" ]]; then 
     echo "On HPC, loading modules"
     module purge
     module load rhel8/default-icl &> /dev/null
+    unset I_MPI_PMI_LIBRARY
 else
     echo "Not on HPC, no modules"
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
 fi
 
 
-unset I_MPI_PMI_LIBRARY
 export OMP_NUM_THREADS=1
 unset PYTHONDONTWRITEBYTECODE
 
