ac55a410563c122c0ea147794d8a823ae1a8f16b
James Brind <jb753@cam.ac.uk>
2026-04-06 09:37:42 +0100

    mp

--- src/turbigen/solvers/ember.py
+++ src/turbigen/solvers/ember.py
@@ -41,7 +41,7 @@ class Ember(BaseSolver):
         print(f"xllim: {xllim}")
 
         for patch in grid.patches.outlet:
-            patch.set_adjustment("dynamic_head", K=1.0, rf=1.0)
+            patch.set_adjustment("dynamic_head", K=2.0, rf=1.0)
 
         import matplotlib.pyplot as plt
 
@@ -70,11 +70,11 @@ class Ember(BaseSolver):
             sf4=0.01,
             sf2_adapt=2.0,
             rf_inlet=2.0,
-            rf_outlet=0.5,
+            rf_outlet=1.0,
             delta_filt=1.0,
             fac_gain_filt=0.8,
-            rf_mixing=1.0,
-            # fac_smooth_coarse=0.5,
+            rf_mixing=0.10,
+            fac_smooth_coarse=1.0,
             inviscid=False,
             shear_work=True,
             full_mgrid=True,
